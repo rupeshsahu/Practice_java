12aa99e2397a49b276e4626e5d636b35b53cad87
Rupesh Sahu <rupeshsahu7509@gmail.com>
2022-06-10 01:35:55 +0530

    10 june 2022

--- src/main/java/LLD_basics/AccessModifiers/pkg1/ClassA.java
+++ src/main/java/LLD_basics/AccessModifiers/pkg1/ClassA.java
@@ -1,4 +1,8 @@
 package LLD_basics.AccessModifiers.pkg1;
 
-public class ClassA {
+public abstract class ClassA {
+
+    int Aage=33;
+    public String Aname="A";
+    public String Adom="22-jun-2022";
 }
--- src/main/java/LLD_basics/AccessModifiers/pkg2/ClassB.java
+++ src/main/java/LLD_basics/AccessModifiers/pkg2/ClassB.java
@@ -1,4 +1,15 @@
 package LLD_basics.AccessModifiers.pkg2;
 
-public class ClassB {
+import LLD_basics.AccessModifiers.pkg1.ClassA;
+
+public class ClassB extends ClassA {
+
+    public static void main(String[] args) {
+        ClassB Bobj=new ClassB();
+        System.out.println(Bobj.Aname+"    "+Bobj.Adom);
+
+    }
+
+
+
 }
