891ccb10b0f1d7b19b6eab238b4b6b11b3e842f7
rupeshsahu <rupeshsahu7509@gmail.com>
2025-05-03 19:12:24 +0530

    3 May 2025 various way to write lambda expression

--- src/main/java/lambda_expression/lambdaexpression.java
+++ src/main/java/lambda_expression/lambdaexpression.java
@@ -4,8 +4,22 @@ public class lambdaexpression {
 
 
     public static void main(String[] args) {
-        lambda l= (int a,int b)->{return a+b;};
-        System.out.println(l.m1(4,5));
+        lambda l1 = (a, b) -> a + b;
+
+        // With braces and return
+        lambda l2 = (a, b) -> { return a + b; };
+
+        // With type declaration
+        lambda l3 = (int a, int b) -> a + b;
+
+        // Full form
+        lambda l4 = (int a, int b) -> { return a + b; };
+
+        // Output
+        System.out.println(l1.m1(4, 5));
+        System.out.println(l2.m1(4, 5));
+        System.out.println(l3.m1(4, 5));
+        System.out.println(l4.m1(4, 5));
 
 
 
